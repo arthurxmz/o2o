48d84501f020ee584721f8674f300652ae6d1e93
xieminghui <xmh594603296@163.com>
2019-06-18 14:35:11 +0800

    提交了service

---
+++ src/main/java/com/xiemingzhong/controller/services/UserService.java
@@ -0,0 +1,11 @@
+package com.xiemingzhong.controller.services;
+
+/**
+ * describe:
+ *
+ * @author sunnyhuihui
+ * @email xmh594603296@163.com
+ * @date 2019-06-18 14:34
+ */
+public class UserService {
+}
