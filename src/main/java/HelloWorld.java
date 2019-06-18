10a191d6b4fe3709ad50118a84f8356a44690a03
arthurxmz <1062823068@qq.com>
2019-06-18 14:02:08 +0800

    切换dev

--- src/main/java/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld {
-    public static void hello(){
-        System.out.println("helloworld");
-    }
-
-    public static void main(String [] args){
-        hello();
-    }
-}
--- src/main/java/com/xiemingzhong/controller/UserController.java
+++ src/main/java/com/xiemingzhong/controller/UserController.java
@@ -1,4 +1,11 @@
 package com.xiemingzhong.controller;
 
 public class UserController {
+    public static void main(String [] args){
+        UserController hi=new UserController();
+        hi.hello();
+    }
+    public static void hello(){
+        System.out.println("HelloWorld");
+    }
 }
